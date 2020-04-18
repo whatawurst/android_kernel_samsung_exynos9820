cf9be590394a896550933f469a813c8225d0bb3a
derfelot <derfelot.git@gmail.com>
2020-04-18 02:37:11 +0200

    drivers: video: fbdev: Add Samsung exynos support

--- drivers/video/fbdev/core/fbmon.c
+++ drivers/video/fbdev/core/fbmon.c
@@ -33,6 +33,10 @@
 #include <video/edid.h>
 #include <video/of_videomode.h>
 #include <video/videomode.h>
+#ifdef CONFIG_EXYNOS_DISPLAYPORT
+#include <media/v4l2-dv-timings.h>
+#include <uapi/linux/v4l2-dv-timings.h>
+#endif
 #include "../edid.h"
 
 /*
@@ -491,6 +495,42 @@ static int get_est_timing(unsigned char *block, struct fb_videomode *mode)
 	return num;
 }
 
+#ifdef CONFIG_EXYNOS_DISPLAYPORT
+static struct ext_std_timing_t {
+	u32 byte_code;
+	struct v4l2_dv_timings timing;
+} ext_std_timing[] = {
+	{0xd1c0, V4L2_DV_BT_CEA_1920X1080P60},
+	{0x81c0, V4L2_DV_BT_CEA_1280X720P60},
+};
+
+static int check_ext_std_timing(unsigned char *block, struct fb_videomode *mode)
+{
+	int cnt = ARRAY_SIZE(ext_std_timing);
+	int i;
+	u32 byte_code = block[0]<<8 | block[1];
+
+	for (i = 0; i < cnt; i++)
+		if (byte_code == ext_std_timing[i].byte_code)
+			break;
+
+	if (i < cnt) {
+		mode->right_margin = ext_std_timing[i].timing.bt.hfrontporch;
+		mode->hsync_len = ext_std_timing[i].timing.bt.hsync;
+		mode->left_margin = ext_std_timing[i].timing.bt.hbackporch;
+		mode->lower_margin = ext_std_timing[i].timing.bt.vfrontporch;
+		mode->vsync_len = ext_std_timing[i].timing.bt.vsync;
+		mode->upper_margin = ext_std_timing[i].timing.bt.vbackporch;
+		mode->pixclock = KHZ2PICOS(ext_std_timing[i].timing.bt.pixelclock/1000);
+		pr_info("EDID: found ext std timing %d 2byte:0x%X\n", i, byte_code);
+
+		return 0;
+	}
+
+	return -EINVAL;
+}
+#endif
+
 static int get_std_timing(unsigned char *block, struct fb_videomode *mode,
 			  int ver, int rev, const struct fb_monspecs *specs)
 {
@@ -537,7 +577,18 @@ static int get_std_timing(unsigned char *block, struct fb_videomode *mode,
 		refresh = (block[1] & 0x3f) + 60;
 		DPRINTK("      %dx%d@%dHz\n", xres, yres, refresh);
 
+#ifdef CONFIG_EXYNOS_DISPLAYPORT
+		if (!check_ext_std_timing(block, mode)) {
+			mode->xres = xres;
+			mode->yres = yres;
+			mode->refresh = refresh;
+
+			return 1;
+		} else
+			calc_mode_timings(xres, yres, refresh, mode);
+#else
 		calc_mode_timings(xres, yres, refresh, mode);
+#endif
 	}
 
 	/* Check the mode we got is within valid spec of the monitor */
@@ -997,6 +1048,99 @@ void fb_edid_to_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 	DPRINTK("========================================\n");
 }
 
+/**
+ * fb_edid_add_monspecs() - add monitor video modes from E-EDID data
+ * @edid:	128 byte array with an E-EDID block
+ * @spacs:	monitor specs to be extended
+ */
+void fb_edid_add_monspecs(unsigned char *edid, struct fb_monspecs *specs)
+{
+	unsigned char *block;
+	struct fb_videomode *m;
+	int num = 0, i;
+	u8 svd[64], edt[(128 - 4) / DETAILED_TIMING_DESCRIPTION_SIZE];
+	u8 pos = 4, svd_n = 0;
+
+	if (!edid)
+		return;
+
+	if (!edid_checksum(edid))
+		return;
+
+	if (edid[0] != 0x2 ||
+	    edid[2] < 4 || edid[2] > 128 - DETAILED_TIMING_DESCRIPTION_SIZE)
+		return;
+
+	DPRINTK("  Short Video Descriptors\n");
+
+	while (pos < edid[2]) {
+		u8 len = edid[pos] & 0x1f, type = (edid[pos] >> 5) & 7;
+		pr_debug("Data block %u of %u bytes\n", type, len);
+
+		pos++;
+		if (type == 2) {
+			for (i = pos; i < pos + len; i++) {
+				u8 idx = edid[i] & 0x7f;
+				svd[svd_n++] = idx;
+				pr_debug("N%sative mode #%d\n",
+					 edid[i] & 0x80 ? "" : "on-n", idx);
+			}
+		} else if (type == 3 && len >= 3) {
+			/* Check Vendor Specific Data Block.  For HDMI,
+			   it is always 00-0C-03 for HDMI Licensing, LLC. */
+			if (edid[pos] == 3 && edid[pos + 1] == 0xc &&
+			    edid[pos + 2] == 0)
+				specs->misc |= FB_MISC_HDMI;
+		}
+		pos += len;
+	}
+
+	block = edid + edid[2];
+
+	DPRINTK("  Extended Detailed Timings\n");
+
+	for (i = 0; i < (128 - edid[2]) / DETAILED_TIMING_DESCRIPTION_SIZE;
+	     i++, block += DETAILED_TIMING_DESCRIPTION_SIZE)
+		if (PIXEL_CLOCK != 0)
+			edt[num++] = block - edid;
+
+	/* Yikes, EDID data is totally useless */
+	if (!(num + svd_n))
+		return;
+
+	m = kzalloc((specs->modedb_len + num + svd_n) *
+		       sizeof(struct fb_videomode), GFP_KERNEL);
+
+	if (!m)
+		return;
+
+	memcpy(m, specs->modedb, specs->modedb_len * sizeof(struct fb_videomode));
+
+	for (i = specs->modedb_len; i < specs->modedb_len + num; i++) {
+		get_detailed_timing(edid + edt[i - specs->modedb_len], &m[i]);
+		if (i == specs->modedb_len)
+			m[i].flag |= FB_MODE_IS_FIRST;
+		pr_debug("Adding %ux%u@%u\n", m[i].xres, m[i].yres, m[i].refresh);
+	}
+
+	for (i = specs->modedb_len + num; i < specs->modedb_len + num + svd_n; i++) {
+		int idx = svd[i - specs->modedb_len - num];
+		if (!idx || idx >= ARRAY_SIZE(cea_modes)) {
+			pr_warn("Reserved SVD code %d\n", idx);
+		} else if (!cea_modes[idx].xres) {
+			pr_warn("Unimplemented SVD code %d\n", idx);
+		} else {
+			memcpy(&m[i], cea_modes + idx, sizeof(m[i]));
+			pr_debug("Adding SVD #%d: %ux%u@%u\n", idx,
+				 m[i].xres, m[i].yres, m[i].refresh);
+		}
+	}
+
+	kfree(specs->modedb);
+	specs->modedb = m;
+	specs->modedb_len = specs->modedb_len + num + svd_n;
+}
+
 /*
  * VESA Generalized Timing Formula (GTF)
  */
@@ -1406,6 +1550,9 @@ int fb_parse_edid(unsigned char *edid, struct fb_var_screeninfo *var)
 void fb_edid_to_monspecs(unsigned char *edid, struct fb_monspecs *specs)
 {
 }
+void fb_edid_add_monspecs(unsigned char *edid, struct fb_monspecs *specs)
+{
+}
 void fb_destroy_modedb(struct fb_videomode *modedb)
 {
 }
@@ -1513,6 +1660,7 @@ EXPORT_SYMBOL(fb_firmware_edid);
 
 EXPORT_SYMBOL(fb_parse_edid);
 EXPORT_SYMBOL(fb_edid_to_monspecs);
+EXPORT_SYMBOL(fb_edid_add_monspecs);
 EXPORT_SYMBOL(fb_get_mode);
 EXPORT_SYMBOL(fb_validate_mode);
 EXPORT_SYMBOL(fb_destroy_modedb);
