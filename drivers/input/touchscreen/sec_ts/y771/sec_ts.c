970129d29d2ab62a663bbfa9cbbc1dd8ccb374e6
Andreas Schneider <asn@cryptomilk.org>
2020-06-14 11:15:50 +0200

    drivers:input:ts: Apply Samsung G970FXXU7CTF1 changes for sec_ts
    Change-Id: Iee7f955df04a341b6eacc245d6528466da9ce149

--- drivers/input/touchscreen/sec_ts/y771/sec_ts.c
+++ drivers/input/touchscreen/sec_ts/y771/sec_ts.c
@@ -3066,8 +3066,7 @@ static int sec_ts_pm_suspend(struct device *dev)
 
 out:
 #endif
-	if (ts->lowpower_mode)
-		reinit_completion(&ts->resume_done);
+	reinit_completion(&ts->resume_done);
 
 	return 0;
 }
@@ -3076,8 +3075,7 @@ static int sec_ts_pm_resume(struct device *dev)
 {
 	struct sec_ts_data *ts = dev_get_drvdata(dev);
 
-	if (ts->lowpower_mode)
-		complete_all(&ts->resume_done);
+	complete_all(&ts->resume_done);
 
 	return 0;
 }
